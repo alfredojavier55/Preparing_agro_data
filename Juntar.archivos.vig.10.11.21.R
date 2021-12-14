0bab69410bc6857c77e85c35d940c24a951bf684
Alfredo A <68718651+alfredojavier55@users.noreply.github.com>
2021-12-14 17:30:31 -0300

    Update Juntar.archivos.vig.10.11.21.R
    Filtros para obtener notificaciones y casos y gráficos

--- Juntar.archivos.vig.10.11.21.R
+++ Juntar.archivos.vig.10.11.21.R
@@ -1,15 +1,24 @@
-# Juntar archivos vigilancia (join-vig)
+# Codigo juntar archivos sizse Vigilancia
+# Estimacion de la sensibilidad del sistema de vigilancia epidemiologica para PPC
+#Alfredo Acosta
+# alfredojavier55@gmail.com
+# Phd Candidate University of Sao Paulo
+# Preventive veterinary medicine department
+#https://github.com/alfredojavier55/Preparing_agro_data
+
+# Juntar archivos vigilancia (join-vig) ----
 # 9.11.21
 
 setwd("~/Dropbox/0.USP/1 Projeto/Conferir-dados/2021")
+library(dplyr);library(lubridate)
 
-# Colnames correctos
+# Colnames correctos ----
 vge0 <- read.csv("Rep_General_EventosSan0.csv", colClasses = "character",encoding = "UTF-8")
 vr0 <- read.csv("Rep_ResultadosGEV0.csv", colClasses = "character",encoding = "UTF-8")
 vc0 <- read.csv("Rep_CierreGEV0.csv", colClasses = "character",encoding = "UTF-8")
 
-# Archivos de general (2014 missing, uso 2015 por ahora)
-vge1 <- readxl::read_excel("VEPG02_REP_GENERAL_BASICO 2015.xls")
+# Archivos de general ----
+vge1 <- readxl::read_excel("VEPG02_REP_GENERAL_BASICO 2014.xls")
 # vge1 <- vge1[-1,]
 vge2 <- readxl::read_excel("VEPG02_REP_GENERAL_BASICO 2015.xls")
 vge2 <- vge2[-1,]
@@ -48,7 +57,7 @@ colnames(vge) <- colnames(vge0)
 
 vge <- vge[-1,]
 
-# Archivos de resultados
+# Archivos de resultados ----
 vr1 <- readxl::read_excel("VEPG02_REP_RESULTADOS 2014.xls")
 # vr1 <- vr1[-1,]
 vr2 <- readxl::read_excel("VEPG02_REP_RESULTADOS 2015.xls")
@@ -77,7 +86,7 @@ colnames(vr) <- colnames(vr0)
 
 vr <- vr[-1,]
 
-# Archivos de cierre
+# Archivos de cierre ----
 vc1 <- readxl::read_excel("VEPG02_REP_CIERRE 2014.xls")
 # vc1 <- vc1[-1,]
 vc2 <- readxl::read_excel("VEPG02_REP_CIERRE 2015.xls")
@@ -102,7 +111,7 @@ colnames(vc) <- vc1[1,]
 colnames(vc0)
 colnames(vc)
 
-# Revisar cristian ----
+# Reordenar ----
 vc <- vc[,c(1,7:9,11,12,4,10,10,10,13:20,
             22,23,3,3,5,36,26:35)]
 
@@ -136,12 +145,189 @@ vc$sacrificad <- as.numeric(vc$sacrificad)
 vc$canton <- vc$cantón
 vc$cantón <- NULL
 
-#3 Linkage dos reportes ----
-v0 <- full_join(vc, vr)
-v1 <- full_join(v0,vge)
+# -- Vigilancia general porcinos ----
+v0 <- left_join(vc, vr)
+v1 <- left_join(v0,vge)
 
-# Modificando formato de fecha
-v1$f_1er_enfermo <- dmy(v1$f_1er_enfermo) # "01/08/2015"
-v1$detalle_diagnóstico <- tolower(iconv(v1$detalle_diagnóstico, to='ASCII//TRANSLIT'))
-v1$prueba_solicitada <- tolower(iconv(v1$prueba_solicitada, to='ASCII//TRANSLIT'))
+table(v1$especie)
+table(v1$prueba_solicitada)
+
+# Criando os casos
+v2 <- v1 %>%
+  filter(especie == "PORCINOS") 
+
+#agregando ano
+v2$ano <- year(dmy(v2$f_1er_enfermo))
+v2$month <- month(dmy(v2$f_1er_enfermo))
+
+# Borrar bovinos
+v2 <- v2[v2$orden != "1",] #0700860679 loja paltas casamba
+v2 <- v2[v2$orden != "2382",] 
+v2 <- v2[v2$orden != "1096",] 
+v2 <- v2[v2$orden != "4114",] 
+v2 <- v2[v2$orden != "6270",] 
+
+# Numero total de eventos
+length(unique(v2$orden))
+#1758 com atualizacao 10/10/2021 todas as patologias
+#1863 com atualizacao 10/12/2021 todas as patologias
+
+# -- Vigilancia geral suinos ----
+# numero de notificaciones
+v2 %>%
+  group_by(ano)%>%
+  summarise(notifi=length(unique(orden)))
+
+
+#7 -- Vigilancia especifica PPC----
+#criando o v2 novo com as notificações extra
+# Linkage dos reportes
+v0 <- left_join(vc, vr)
+v1 <- left_join(v0,vge)
+
+table(v1$especie)
+table(v1$prueba_solicitada)
+
+# Criando os casos
+v2 <- v1 %>%
+  filter(especie == "PORCINOS") 
+
+v2 <- v2 %>%
+  filter(prueba_solicitada != "PESTE PORCINA CLÁSICA (AC.)")
+###
+v2 <- v2 %>%
+  # filter(detalle_diagnóstico == "Peste porcina clásica")%>%
+  filter(detalle_diagnóstico == "PESTE PORCINA CLÁSICA")%>%
+  group_by(orden, provincia, canton, parroquia, cedula, propietario, semana, zona, 
+           coord_x, coord_y, predio,t_explotación, notificador, 
+           f_1er_enfermo, f_notificación, f_1era_visita, síndrome_presuntivo,
+           patología, especie, edad, f_elaboración, f_ingreso, f_cierre_orden,
+           responsable, vacuno, focal, dosis_focal, perifocal, dosis_perifocal, 
+           especie_f, colecta)%>%
+  summarise(existente=sum(existentes, muertos, sacrificad), enfermo=sum(enfermos), mortos=sum(muertos), 
+            sacrifi=sum(sacrificad), afetados=sum(muertos,sacrificad), 
+            pos=sum(positivos), total_muestras=sum(cant_muestras), 
+            indeterm=sum(indeterminados), reactivo=sum(reactivos))
+
+#Quando faço este mesto filtro ao tirar o detalhe diagnostico de PPC
+#qualquer outra fica como que se fosse ppc e isso nao e certo.
+# inclui prueba solicitada, os casos são submetidos a varias provas, estou registrando
+# o primeiro que sai desde o joint com os resultados
+
+plot(v2$existente)
+hist(log(v2$existente))
+table(v2$existente)
+summary(v2$existente)
+
+#agregando ano
+v2$ano <- year(dmy(v2$f_1er_enfermo))
+v2$month <- month(dmy(v2$f_1er_enfermo))
+
+# Borrar bovinos
+v2 <- v2[v2$orden != "1",] #0700860679 loja paltas casamba
+v2 <- v2[v2$orden != "2382",] 
+v2 <- v2[v2$orden != "1096",] 
+v2 <- v2[v2$orden != "4114",] 
+v2 <- v2[v2$orden != "6270",] 
+
+# Numero total de eventos
+length(unique(v2$orden))
+#1089 com atualizacao 10/12/2021 todas as patologias
+
+# 5 -- Vigilancia geral suinos ----
+# numero de notificaciones
+v2 %>%
+  group_by(ano)%>%
+  summarise(notifi=length(unique(orden)))
+
+# Numero de casos e notificacoes ----
+v2 %>%
+  group_by(ano)%>%
+  # mutate(caso=ifelse(pos>=1,"caso", "control"))%>%
+  # filter(ano == "2021")%>%
+  summarise(notifi_PPC=length(unique(orden)),
+            surtos_PPC=sum(pos >= 1, na.rm = TRUE))
+
+# Cargando librerias de color
+library(RColorBrewer)
+display.brewer.pal(n=8,name="Set1")
+brewer.pal(n=8,name="Set1")
+show_col(viridis_pal()(4))
+
+
+# Asignando columna de fechas para graficos
+v2$f_1er_enfermo <- dmy(v2$f_1er_enfermo)
+# Changing to floor date week
+v2$week <- floor_date(v2$f_1er_enfermo, "week")
+# Best visualizations by month
+v2$Month <- floor_date(v2$f_1er_enfermo, "month")
+
+# Graficos
+v2 %>%
+  # group_by(month)%>%
+  group_by(Month)%>%
+  # filter(ano <2020)%>%
+  # filter(ano >2016)%>%
+  summarise(notifi_geral=length(unique(orden)),
+            casos=sum(pos >= 1, na.rm = TRUE))%>%
+  ggplot()+
+  geom_col(aes(Month,notifi_geral), fill="#377EB8")+
+  scale_y_continuous(breaks= pretty_breaks())+
+  labs(y="Notificaciones vigilancia general",
+       x="Meses")+
+  theme_minimal() +
+  theme(text = element_text(size = 14))
+
+# notificaciones por semana epidemiologica
+v2 %>%
+  # group_by(month)%>%
+  group_by(week)%>%
+  # filter(ano <2020)%>%
+  # filter(ano >2016)%>%
+  summarise(notifi_geral=length(unique(orden)),
+            diagnosticados=sum(pos >= 1, na.rm = TRUE))%>%
+  ggplot()+
+  geom_col(aes(week,notifi_geral), fill="#377EB8")+
+  scale_y_continuous(breaks= pretty_breaks())+
+  labs(y="Notificaciones por semana ",
+       x="Meses")+
+  theme_minimal() +
+  theme(text = element_text(size = 14))
+
+
+# Casos por meses
+v2 %>%
+  group_by(Month)%>%
+  # group_by(week)%>%
+  # filter(ano <2020)%>%
+  # filter(ano >2016)%>%
+  summarise(notifi_geral=length(unique(orden)),
+            casos=sum(pos >= 1, na.rm = TRUE))%>%
+  ggplot()+
+  geom_col(aes(Month,casos), fill="#377EB8")+
+  scale_y_continuous(breaks= pretty_breaks())+
+  labs(y="Casos PPC",
+       x="Meses")+
+  theme_minimal() +
+  theme(text = element_text(size = 14))
+
+
+# Casos por meses (filtrado un ano)
+v2 %>%
+  group_by(Month)%>%
+  # group_by(week)%>%
+  filter(ano >2019)%>%
+  # filter(ano >2016)%>%
+  summarise(notifi_geral=length(unique(orden)),
+            casos=sum(pos >= 1, na.rm = TRUE))%>%
+  ggplot()+
+  geom_col(aes(Month,casos), fill="#377EB8")+
+  scale_y_continuous(breaks= pretty_breaks())+
+  labs(y="Casos PPC",
+       x="Meses")+
+  theme_minimal() +
+  theme(text = element_text(size = 14))
+
+
+
 
